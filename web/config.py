9e10643ad33a87067450c2d58d22800d8ec0826e
AFLAH403 <githubtbm@gmail.com>
2025-06-27 08:38:46 +0530

    Update config.py

--- web/config.py
+++ web/config.py
@@ -1,12 +1,13 @@
 ```python
 import os
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-OWNER_ID = int(os.getenv("OWNER_ID"))
-TELEGRAM_API = os.getenv("TELEGRAM_API")
-TELEGRAM_HASH = os.getenv("TELEGRAM_HASH")
-DATABASE_URL = os.getenv("DATABASE_URL")
+
+BOT_TOKEN = os.getenv("")
+OWNER_ID = int(os.getenv("6272165202"))
+TELEGRAM_API = os.getenv("24638343")
+TELEGRAM_HASH = os.getenv("c16b13b73c2f2473b8e3cbcf2ab1d200")
+DATABASE_URL = os.getenv("mongodb+srv://youyooou8:SZSe2ESMc0T8IpLj@cluster0.dpq1fyv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 USER_SESSION_STRING = os.getenv("USER_SESSION_STRING", "")
 DOWNLOAD_DIR = os.getenv("DOWNLOAD_DIR", "/usr/src/app/downloads/")
 CMD_SUFFIX = os.getenv("CMD_SUFFIX", "")
-AUTHORIZED_CHATS = os.getenv("AUTHORIZED_CHATS", "")
+AUTHORIZED_CHATS = os.getenv("AUTHORIZED_CHATS", "-1002708199754")
 ```
